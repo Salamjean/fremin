4b2c30871433956d2db29f93fc978edc0a2706c7
unknown <salamjeanlouis3@gmail.com>
2026-03-24 14:37:11 +0000

    fix:email

--- resources/views/home/layouts/template.blade.php
+++ resources/views/home/layouts/template.blade.php
@@ -126,7 +126,7 @@
                 <p>Deux plateaux, Vallon, non loin du siège de Burida</p>
                 <p>Abidjan, Côte d'Ivoire</p>
                 <p class="mt-3"><strong>Téléphone:</strong> <span>+225 27 22 41 72 92</span></p>
-                <p><strong>Email:</strong> <span>contact@fremin.ci</span></p>
+                <p><strong>Email:</strong> <span>contact@freminci.com</span></p>
               </div>
             </div>
 
